27c6ad1a686215e7bac5a627f44d829b5a4664a7
Bar Benezri <76682634+barbenezri@users.noreply.github.com>
2021-08-13 14:06:58 +0300

    Car Detection

--- VehicleTracking_SafetyRaiting/VehicleTracking_SafetyRaiting.py
+++ VehicleTracking_SafetyRaiting/VehicleTracking_SafetyRaiting.py
@@ -1,38 +1,35 @@
-import cv2
-import numpy as np
+cap = cv2.VideoCapture('Video/3.mp4')
+ret,oldFrame = cap.read()
+oldGrayFrame = cv2.cvtColor(oldFrame, cv2.COLOR_BGR2GRAY) 
+blank1 = np.zeros(oldFrame.shape, np.uint8)
 
-cap = cv2.VideoCapture('Video/1.mp4')
-object_detector = cv2.createBackgroundSubtractorMOG2(history = 100, varThreshold= 100)
-tracker = EuclideanDistTracker()
-
-while True:
-    ret, frame = cap.read()
+while ret:
+    ret, newFrame = cap.read()
     if ret == False: break
-        
-    black = np.zeros_like(frame)
-    mask = object_detector.apply(frame)
-    contours,_ =cv2.findContours(mask, cv2.RETR_TREE, cv2.CHAIN_APPROX_SIMPLE)
-    detection = []
     
-    for cnt in contours:
-        area = cv2.contourArea(cnt)
-        if area > 20:
-            cv2.drawContours(black,[cnt],-1,(255,255,255),1)
-            x,y,w,h = cv2.boundingRect(cnt)
-            detection.append([x,y,w,h])
-    
-    boxes_ids = tracker.update(detection)
-    for box_id in boxes_ids:
-        x,y,w,h,id = box_id
-        cv2.putText(black,str(id), (x,y-15),cv2.FONT_HERSHEY_COMPLEX_SMALL,1,(0,255,0),2)
-        
-    cv2.imshow('frame', frame)
-    cv2.imshow('black', black)
-    
-    
-    if cv2.waitKey(30)==ord('q'):
-        break
+    newGrayFrame = cv2.cvtColor(newFrame, cv2.COLOR_BGR2GRAY) 
+    result = cv2.absdiff(newGrayFrame,oldGrayFrame)
+    blank2 = np.zeros(oldFrame.shape, np.uint8)
 
-    #cv2.imshow('frame', cv2.resize(frame,f(frame,0.7)))  
-    #cv2.imshow('mask', cv2.resize(mask,f(mask,0.7)))
+    if np.mean(result)!=0: 
+        threshold = 30
+        result[result>=threshold]=255
+        result[result<threshold]=0
+        result = cv2.GaussianBlur(result,(7,7),3)
+    
+        contours,_ = cv2.findContours(result,cv2.RETR_TREE, cv2.CHAIN_APPROX_NONE)
+        for cnt in contours:
+            if cv2.contourArea(cnt)>250:
+                cv2.drawContours(blank1,cnt,-1,(0,255,0),2)  
+                cv2.drawContours(blank2,cnt,-1,(0,255,0),2)  
+        cv2.imshow('result',cv2.resize(result,(640,400)))  
+        cv2.imshow('newFrame',cv2.resize(newFrame,(640,400)))   
+        cv2.imshow('blank1',cv2.resize(blank1,(640,400)))   
+        cv2.imshow('blank2',cv2.resize(blank2,(640,400)))   
+    
+    oldGrayFrame = newGrayFrame
+    
+    if cv2.waitKey(1)==ord('q'):
+        break
+cap.release()
 cv2.destroyAllWindows()    
